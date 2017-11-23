9224c80ad66561e24be9c1a7509ca706c6e3994f
highkix <XXX>
2017-11-24 00:35:31 +0100

    add babel-cli + yarn start script

--- src/server/index.js
+++ src/server/index.js
@@ -6,8 +6,17 @@ import routing from './routing'
 
 
 const app = express()
+const http = Server(app)
 
 app.use(compression())
 app.use(favicon('public/img/favicon.ico'))
+app.use(STATIC_PATH, express.static('dist'))
+app.use(STATIC_PATH, express.static('public'))
 
 routing(app)
+
+http.listen(WEB_PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${String(WEB_PORT)} ${isProd ? '(production)' :
+    '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
+})
