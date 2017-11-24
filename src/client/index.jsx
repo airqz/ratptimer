a01c0264a71b465e464ed6d227b2097a815bda4c
highkix <XXX>
2017-11-24 01:13:25 +0100

    fix errors

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,5 @@
 
-import Immutable from 'immutable'
+/*import Immutable from 'immutable'*/
 import $ from 'jquery'
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -7,5 +7,3 @@ import ReactDOM from 'react-dom'
 import App from '../shared/app'
 
 window.jQuery = $
-window.Tether = Tether
-require('bootstrap')
