55eaceca2f8d17fbeb81bb4ef612510e725889a2
highkix <XXX>
2017-11-24 01:51:12 +0100

    fix render-app

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -13,15 +13,12 @@ const renderApp = () => {
         <meta charset="utf-8">
         ${head.title}
         ${head.meta}
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width">
         <link rel="stylesheet" href="${STATIC_PATH}/css/bootstrap.min.css">
         <style class="${JSS_SSR_CLASS}">${sheets.toString()}</style>
       </head>
       <body>
         <div class="${APP_CONTAINER_CLASS}">${appHtml}</div>
-        <script>
-          window.__PRELOADED_STATE__ = ${JSON.stringify(store.getState())}
-        </script>
         <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
       </body>
     </html>`
