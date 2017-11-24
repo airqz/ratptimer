bf0c0af8eca955d7f8847395c9cf303d96f451d4
highkix <XXX>
2017-11-24 05:36:54 +0100

    change app name

--- src/shared/component/nav.jsx
+++ src/shared/component/nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => (
         { route: HELLO_PAGE_ROUTE, label: 'Say Hello' }
       ].map(link => (
         <li className="nav-item" key={link.route}>
-          <NavLink to={link.route} className="nav-link" activeStyle={{ color: 'white' }} exact onClick={handleNavLinkClick}>{link.label}</NavLink>
+          <NavLink to={link.route} className="nav-link" activeStyle={{ color: 'green' }} exact onClick={handleNavLinkClick}>{link.label}</NavLink>
         </li>
       ))}
     </ul>
