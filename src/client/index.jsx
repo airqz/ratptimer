f7b117c2f11a2915638693520f99e5ede537afba
highkix <XXX>
2017-11-24 01:39:57 +0100

    add boostrap + tether packages + improve client index

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,7 +3,27 @@
 import $ from 'jquery'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import Tether from 'tether'
 
 import App from '../shared/app'
 
 window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
+
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
+
+const wrapApp = (AppComponent, reduxStore) =>
+  <AppContainer>
+    <AppComponent />
+  </AppContainer>
+
+ReactDOM.render(wrapApp(App), rootEl)
+
+if (module.hot) {
+  module.hot.accept('../shared/app', () => {
+    const NextApp = require('../shared/app').default
+    ReactDOM.render(wrapApp(NextApp), rootEl)
+  })
+}
