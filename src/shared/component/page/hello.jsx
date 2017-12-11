cb96205b039047a492bf77459cb8a7545d36c7e1
highkix <XXX>
2017-12-11 21:29:25 +0100

    test display img

--- src/shared/component/page/hello.jsx
+++ src/shared/component/page/hello.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 const HelloPage = () => (
   <div className="">
     <p>Hello</p>
+    <img src="http://opendata-tr.ratp.fr/wsiv/static/line/p_met.gif" alt="" />
   </div>
 )
 
